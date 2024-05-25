cac86495a95dcedf0151f6074bb2d71b7d97dd2a
khmalz <akmalrma1710@gmail.com>
2024-05-25 17:20:32 +0700

    update vercel

--- lib/firestore/init.ts
+++ lib/firestore/init.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-   apiKey: Bun.env.FIREBASE_API_KEY,
-   authDomain: Bun.env.FIREBASE_AUTH_DOMAIN,
-   projectId: Bun.env.FIREBASE_PROJECT_ID,
-   storageBucket: Bun.env.FIREBASE_STORAGE_BUCKET,
-   messagingSenderId: Bun.env.FIREBASE_MESSAGING_SENDER_ID,
-   appId: Bun.env.FIREBASE_APP_ID,
+   apiKey: process.env.FIREBASE_API_KEY,
+   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+   projectId: process.env.FIREBASE_PROJECT_ID,
+   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
