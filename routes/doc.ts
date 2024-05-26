dae46665f435e2247bb947878c5d954d29252a94
khmalz <akmalrma1710@gmail.com>
2024-05-26 18:24:09 +0700

    trying add openapi

--- routes/doc.ts
+++ routes/doc.ts
@@ -1,9 +1,17 @@
 import { swaggerUI } from "@hono/swagger-ui";
-import { Hono } from "hono";
+import { OpenAPIHono } from "@hono/zod-openapi";
 
-const doc = new Hono();
+const doc = new OpenAPIHono();
+
+doc.doc("/", {
+   openapi: "3.1.0",
+   info: {
+      version: "1.0.0",
+      title: "Football Quiz API",
+   },
+});
 
 // @ts-ignore
-doc.get("/ui", swaggerUI({ url: "/", version: "3.1.0" }));
+doc.get("/ui", swaggerUI({ url: "/doc" }));
 
 export default doc;
