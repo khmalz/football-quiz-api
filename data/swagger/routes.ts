5dfed6537cd52d91492461b39895effe30119607
khmalz <akmalrma1710@gmail.com>
2024-05-26 21:06:15 +0700

    using pure swagger-ui

--- data/swagger/routes.ts
+++ data/swagger/routes.ts
@@ -1,158 +1,172 @@
-import { createRoute, z } from "@hono/zod-openapi";
-
-export const sayHello = createRoute({
-   tags: ["Testing"],
-   method: "get",
-   path: "/api/hello",
-   request: {
-      query: z.object({
-         name: z.string().openapi({
-            param: {
-               name: "name",
-               in: "query",
-               required: false,
-            },
-            type: "String",
-            example: "John",
-         }),
-      }),
+export const openApiSpec = {
+   openapi: "3.1.0",
+   info: {
+      version: "1.0.0",
+      title: "Football Quiz API",
    },
-   responses: {
-      200: {
-         content: {
-            "application/json": {
-               schema: z.object({
-                  data: z.string().default("Hello, World!"),
-               }),
+   paths: {
+      "/api/hello": {
+         get: {
+            summary: "Testing API",
+            tags: ["Testing"],
+            parameters: [
+               {
+                  name: "name",
+                  in: "query",
+                  description: "Name of the user",
+                  required: false,
+                  example: "John",
+               },
+            ],
+            responses: {
+               200: {
+                  description: "Hello response",
+                  content: {
+                     "application/json": {
+                        schema: {
+                           type: "object",
+                           properties: {
+                              data: {
+                                 type: "string",
+                                 example: "Hello, John!",
+                              },
+                           },
+                        },
+                     },
+                  },
+               },
             },
          },
-         description: "Successful response",
       },
-      500: {
-         content: {
-            "application/json": {
-               schema: z.object({
-                  status: z.boolean().default(false),
-                  statusCode: z.number().default(500),
-                  message: z.string().default("Internal Server Error"),
-               }),
+      "/api/questions/category/{category}": {
+         get: {
+            summary: "List questions by category",
+            tags: ["Questions"],
+            parameters: [
+               {
+                  name: "category",
+                  in: "path",
+                  required: true,
+                  description: "Available values: championsleague, premierleague, laliga",
+                  example: "championsleague",
+               },
+            ],
+            responses: {
+               200: {
+                  description: "Questions response",
+                  content: {
+                     "application/json": {
+                        schema: {
+                           type: "object",
+                           properties: {
+                              status: {
+                                 type: "boolean",
+                                 description: "Success status",
+                              },
+                              statusCode: {
+                                 type: "integer",
+                                 description: "Success status code",
+                                 default: 200,
+                              },
+                              data: {
+                                 type: "object",
+                                 properties: {
+                                    type: { type: "string" },
+                                    questions: {
+                                       type: "array",
+                                       items: {
+                                          type: "object",
+                                          properties: {
+                                             question: { type: "string" },
+                                             options: {
+                                                type: "string",
+                                                example: ["Option 1", "Option 2", "Option 3", "Option 4"],
+                                             },
+                                             answer: { type: "string" },
+                                          },
+                                       },
+                                    },
+                                 },
+                              },
+                           },
+                        },
+                     },
+                  },
+               },
+               400: {
+                  description: "Invalid category",
+                  content: {
+                     "application/json": {
+                        schema: {
+                           type: "object",
+                           properties: {
+                              status: {
+                                 type: "boolean",
+                                 description: "Success status",
+                                 default: false,
+                              },
+                              statusCode: {
+                                 type: "integer",
+                                 description: "Success status code",
+                                 default: 400,
+                              },
+                              message: { type: "string", default: "Invalid param" },
+                           },
+                        },
+                     },
+                  },
+               },
+               404: {
+                  description: "Not found",
+                  content: {
+                     "application/json": {
+                        schema: {
+                           type: "object",
+                           properties: {
+                              status: {
+                                 type: "boolean",
+                                 description: "Success status",
+                                 default: false,
+                              },
+                              statusCode: {
+                                 type: "integer",
+                                 description: "Success status code",
+                                 default: 404,
+                              },
+                              data: {
+                                 type: "object",
+                                 default: {},
+                              },
+                           },
+                        },
+                     },
+                  },
+               },
+               500: {
+                  description: "Internal server error",
+                  content: {
+                     "application/json": {
+                        schema: {
+                           type: "object",
+                           properties: {
+                              status: {
+                                 type: "boolean",
+                                 description: "Success status",
+                                 default: false,
+                              },
+                              statusCode: {
+                                 type: "integer",
+                                 description: "Success status code",
+                                 default: 500,
+                              },
+                              message: { type: "string", default: "Something went wrong" },
+                           },
+                        },
+                     },
+                  },
+               },
             },
          },
-         description: "Internal server error response",
       },
    },
-});
-
-export const testingConnection = createRoute({
-   tags: ["Testing"],
-   method: "get",
-   path: "/api/users",
-   responses: {
-      200: {
-         content: {
-            "application/json": {
-               schema: z.object({
-                  status: z.boolean(),
-                  statusCode: z.number().default(400),
-                  data: z.array(
-                     z.object({
-                        id: z.string(),
-                        name: z.string(),
-                     })
-                  ),
-               }),
-            },
-         },
-         description: "Successful response",
-      },
-      404: {
-         content: {
-            "application/json": {
-               schema: z.object({
-                  status: z.boolean().default(false),
-                  statusCode: z.number().default(404),
-                  data: z.object({}),
-               }),
-            },
-         },
-         description: "Not found response",
-      },
-      500: {
-         content: {
-            "application/json": {
-               schema: z.object({
-                  status: z.boolean().default(false),
-                  statusCode: z.number().default(500),
-                  message: z.string().default("Internal Server Error"),
-               }),
-            },
-         },
-         description: "Internal server error response",
-      },
-   },
-});
-
-export const questionsRoute = createRoute({
-   tags: ["Questions"],
-   method: "get",
-   path: "/api/questions/category/{category}",
-   request: {
-      params: z.object({
-         category: z.string().openapi({
-            param: {
-               name: "category",
-               in: "path",
-               description: "Available values: championsleague, premierleague, laliga",
-            },
-            type: "string",
-         }),
-      }),
-   },
-   responses: {
-      200: {
-         content: {
-            "application/json": {
-               schema: z.object({
-                  status: z.boolean(),
-                  statusCode: z.number().default(200),
-                  data: z.object({
-                     type: z.string(),
-                     questions: z.array(
-                        z.object({
-                           question: z.string(),
-                           options: z.array(z.string()),
-                           answer: z.string(),
-                        })
-                     ),
-                  }),
-               }),
-            },
-         },
-         description: "Successful response",
-      },
-      400: {
-         content: {
-            "application/json": {
-               schema: z.object({
-                  status: z.boolean().default(false),
-                  statusCode: z.number().default(400),
-                  message: z.string().default("Invalid param"),
-               }),
-            },
-         },
-         description: "Bad request response",
-      },
-      404: {
-         content: {
-            "application/json": {
-               schema: z.object({
-                  status: z.boolean().default(false),
-                  statusCode: z.number().default(404),
-                  data: z.object({}),
-               }),
-            },
-         },
-         description: "Not found response",
-      },
-   },
-});
+};
--- routes/doc.ts
+++ routes/doc.ts
@@ -1,9 +1,23 @@
 import { swaggerUI } from "@hono/swagger-ui";
 import { Hono } from "hono";
 
+import { openApiSpec } from "../data/swagger/routes";
+
 const doc = new Hono();
 
+const DisableTryItOutPlugin = function () {
+   return {
+      statePlugins: {
+         spec: {
+            wrapSelectors: {
+               allowTryItOutFor: () => () => false,
+            },
+         },
+      },
+   };
+};
+
 // @ts-ignore
-doc.get("/ui", swaggerUI({ url: "/", version: "3.1.0" }));
+doc.get("/ui", swaggerUI({ url: "/", version: "3.1.0", spec: openApiSpec, plugins: [DisableTryItOutPlugin] }));
 
 export default doc;
